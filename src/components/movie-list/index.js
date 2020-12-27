23a89aae61a58f8dd0a4e70a2ea6e6da04335c94
kunaldoshi98 <68844308+kunaldoshi98@users.noreply.github.com>
2020-12-27 17:01:23 +0530

    try

--- src/components/movie-list/index.js
+++ src/components/movie-list/index.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 import "./index.css";
 
 function MovieList() {
-  const { movie, setMovie } = React.useState();
-  const handleClick = () => {};
-  const handleChange = () => {};
+  let movie = {};
+  let value = 0;
+  const { movieName, setMovieName } = React.useState(0);
+  const handleClick = () => {
+    setMovieName(value);
+  };
+  const handleChange = (event) => {
+    value = event.target.value;
+  };
+  fetch("https://jsonmock.hackerrank.com/api/movies")
+    .then((res) => res.join)
+    .then((json) => (movie = json));
   return (
     <div className="layout-column align-items-center mt-50">
       <section className="layout-row align-items-center justify-content-center">
@@ -21,7 +30,7 @@ function MovieList() {
       </section>
 
       <ul className="mt-50 styled" data-testid="movieList">
-        <li className="slide-up-fade-in py-10">{movie}</li>
+        <li className="slide-up-fade-in py-10">{movieName}</li>
       </ul>
 
       <div className="mt-50 slide-up-fade-in" data-testid="no-result"></div>
